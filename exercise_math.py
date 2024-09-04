6d67aa717585d6b79a4727bacf6369beb5524b85
RondinoniJuan <162723034+RondinoniJuan@users.noreply.github.com>
2024-09-04 18:15:58 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,19 @@
 def math():
     a = 57
     b = 7
+
+    resultado1 = a + b
+    resultado2 = a - b
+    resultado3 = a * b
+    resultado4 = resultado1 / 2
+    resultado5 = a // b
+    resultado6 = a % b
+    resultado7 = a / b
+
+    print (resultado1)
+    print (resultado2)
+    print (resultado3)
+    print (resultado4)
+    print (resultado5)
+    print (resultado6)
+    print (resultado7)
